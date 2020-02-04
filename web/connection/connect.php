485d21d90d894574733ff31dc38522bc2987913a
Leonidas Yopan <leonidasyopan@gmail.com>
2020-02-04 11:10:42 -0300

    Provisory localhost connection

--- web/connection/connect.php
+++ web/connection/connect.php
@@ -5,7 +5,7 @@
 
     if (empty($dbUrl)) {
     // example localhost configuration URL with postgres username and a database called cs313db
-        $dbUrl = exec("heroku config:get DATABASE_URL -a postgresql-rigid-31826");
+        $dbUrl = "postgres://ipnbcebnlrkyfa:a56fe8af10f5c48cc7c9d1842376f900ebdf590be3a467d41cb3785317a7a327@ec2-184-72-235-80.compute-1.amazonaws.com:5432/d3524nt5j73hmb";
     }
 
     $dbopts = parse_url($dbUrl);
