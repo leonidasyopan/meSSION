3d4d7edb8bd768962cf922cf616a3a8cc40dcfbd
Leonidas Yopan <leonidasyopan@gmail.com>
2020-02-03 22:51:01 -0300

    Another test of connection

--- connection/connect.php
+++ connection/connect.php
@@ -1,25 +1,16 @@
 <?php
 
-{
-    $dbUrl = getenv('DATABASE_URL');
-  
-    $dbOpts = parse_url($dbUrl);
-  
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-  
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-  
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 /*
     $dbUrl = exec("heroku config:get DATABASE_URL");
