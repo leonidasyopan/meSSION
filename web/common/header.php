7ed7ddc4299149c4af8553d9a735f65d87422e75
Leonidas Yopan <leonidasyopan@gmail.com>
2020-02-06 11:51:20 -0300

    minor changes to header.php

--- web/common/header.php
+++ web/common/header.php
@@ -2,13 +2,13 @@
     <nav id="top-header">
         <div class="content-width"> <!-- This DIV has only style purposes -->    
             <ul>
-                <li><a href="login.php">Login <i class="fas fa-user"></i></a></li>
+                <li><a href="login.php">Register/Login <i class="fas fa-user"></i></a></li>
             </ul>    
         </div>
     </nav>
     <nav id="main-navigation">
         <div class="content-width" id="logo-ul-holder"> <!-- This DIV has only style purposes -->
-            <p id="logo"><span>me</span>SSION</p>
+            <p id="logo"><a href="index.php"><span>me</span>SSION<a href="index.php"></p>
 
             <ul>
                 <li><a href="add-info.php">Add Info <i class="fas fa-address-book"></i></a></li>
